6e001a587dd5026bc4f6f2fe3f65ee3f8834c5f4
Yogesh Kumar <yogakumar221@gmail.com>
2026-03-04 14:46:56 +0530

    Core Utility: Implement generic API Response and Error handlers

---
+++ backend/src/utils/ApiHandler.js
@@ -0,0 +1,29 @@
+export class ApiResponse {
+  constructor(statusCode, data, message = "Success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
+
+export class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = "Something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
