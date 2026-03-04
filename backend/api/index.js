bbe895451756c410bdf0799691c63e4144b34d9d
Yogesh Kumar <yogakumar221@gmail.com>
2026-03-05 01:06:27 +0530

    debug: catch init error and return in response

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,15 @@
-import app from "../src/app.js";
+let handler;
+let initError = null;
 
-export default app;
+try {
+    const { default: app } = await import("../src/app.js");
+    handler = app;
+} catch (err) {
+    console.error("INIT ERROR:", err);
+    initError = { message: err.message, stack: err.stack };
+    handler = (req, res) => {
+        res.status(503).json({ initError });
+    };
+}
+
+export default handler;
